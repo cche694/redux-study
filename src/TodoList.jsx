2bf5e06400d91e3973c924e6e5478c215e704878
chang__ccge <b449568227@gmail.com>
2020-10-06 04:59:36 +0800

    redux init

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,14 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Input } from "antd";
+import { Button } from "antd";
+import { List } from "antd";
+import store from "./Store";
+
 class TodoList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-        <div>TOdoList</div>
-         );
-    }
+  constructor(props) {
+    super(props);
+    
+    this.state = store.getState();
+    store.subscribe(this.handleStoreChange)
+  }
+  onInputChange(e) {
+    const action = {
+      type: "change_input_value",
+      value: e.target.value,
+    };
+    store.dispatch(action)
+  }
+  handleStoreChange=()=>{
+    console.log('store change')
+    this.setState(store.getState())
+  }
+  render() {
+    return (
+      <div>
+        <Input
+          placeholder="todo"
+          style={{ width: 300 }}
+          value={this.state.inputValue}
+          onChange={(e) => {
+            this.onInputChange(e);
+          }}
+        ></Input>
+        <Button type="primary" style={{ marginLeft: 20 }}>
+          Primary Button
+        </Button>
+        <List
+          style={{ width: 300, marginTop: 20 }}
+          bordered
+          dataSource={this.state.list}
+          renderItem={(item) => <List.Item>{item}</List.Item>}
+        />
+      </div>
+    );
+  }
 }
- 
+
 export default TodoList;
